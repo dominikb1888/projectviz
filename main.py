387fd6f02d30fe6e08e398c04a716c52d19e71fe
Dominik Böhler <dominik.boehler@gmx.net>
2023-11-22 07:15:42 +0000

    added jinja templates

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import json
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 import data as d
 
 app = FastAPI()
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -15,7 +17,7 @@ async def index(request: Request):
     with open("templates/index.html", "r") as file:
         html_content = file.read()
 
-    return HTMLResponse(content=html_content)
+    return templates.TemplateResponse(index.html, data())
 
 @app.get("/data")
 async def data():
